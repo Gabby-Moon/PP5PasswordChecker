6068752020557b83b069f9c89f57d6b7dcf07b99
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-01-21 15:58:31 -0800

    Added special character and space testing

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 document.getElementById('click').onclick = function()
 {
     let testPassword = document.getElementById('password').value;
-    document.getElementById('output').innerHTML = "test";
+    document.getElementById('isValid').innerHTML = "test";
     if(testPassword.length < 8)
     {
-        document.getElementById('output').innerHTML = "Password must be at least 8 characters long.";
+        document.getElementById('outputLength').innerHTML = "Password must be at least 8 characters long.";
+    }
+    else
+    {
+        document.getElementById('outputLength').innerHTML = "";
+    }
+
+    for(let i = 0; testPassword.length > i; i++)
+    {
+        let current = testPassword.charAt(i);
+        let specialChar = "!@#$%^&*()_+=-{}[]|\\\";:?></";
+        if(!specialChar.includes(current))
+        {
+            document.getElementById('outputSpecial').innerHTML = "Password must include at least one special character (!,@,#, ect.).";
+        }
+        else
+        {
+            document.getElementById('outputSpecial').innerHTML = "";
+        }
+    }
+
+    let search = testPassword.search(" ");
+    if(search != -1){
+        document.getElementById('outputSpace').innerHTML = "Password must not contain spaces.";
+    }
+    else
+    {
+        document.getElementById('outputSpace').innerHTML = "";
     }
 }
