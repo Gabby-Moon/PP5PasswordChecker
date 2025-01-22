af98b71639f6474ba6fb6e3ac7bfa34b64a95ca0
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-21 16:24:07 -0800

    Added uppercase check

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 document.getElementById('click').onclick = function()
 {
     let testPassword = document.getElementById('password').value;
-    document.getElementById('output').innerHTML = "test";
+    document.getElementById('isValid').innerHTML = "test";
     if(testPassword.length < 8)
     {
-        document.getElementById('output').innerHTML = "Password must be at least 8 characters long.";
+        document.getElementById('isValid').innerHTML = "Password must be at least 8 characters long.";
     }
+
+
+    // 4. Upper Case Letter
+    let pass = false;
+    
+    for (let i = 0; i < testPassword.length; i++) 
+     {
+          if (testPassword.charAt(i) != testPassword.charAt(i).toLowerCase())
+          {
+               pass = true;
+          } 
+
+     }
+
+     if (pass == false)
+     {
+          document.getElementById('outputUpper').innerHTML = "Password must include at least one uppercase letter.";
+     }
+     else
+     {
+          document.getElementById('outputUpper').innerHTML = "";
+     }
+
+
 }
