c03fd48bd07b25e0fd2472f4f02ea10158417508
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-24 14:34:39 -0800

    Added validation confirmation.

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 document.getElementById('click').onclick = function()
 {
-    let testPassword = document.getElementById('password').value;
-    document.getElementById('isValid').innerHTML = "test";
+    let validationCheck = true;
+    
+     let testPassword = document.getElementById('password').value;
+
     if(testPassword.length < 8)
     {
         document.getElementById('outputLength').innerHTML = "Password must be at least 8 characters long.";
+        validationCheck = false;
     }
     else
     {
         document.getElementById('outputLength').innerHTML = "";
     }
 
+    let tracker = 0;
     for(let i = 0; testPassword.length > i; i++)
     {
         let current = testPassword.charAt(i);
         let specialChar = "!@#$%^&*()_+=-{}[]|\\\";:?></";
         if(!specialChar.includes(current))
         {
-            document.getElementById('outputSpecial').innerHTML = "Password must include at least one special character (!,@,#, ect.).";
+          tracker++;
         }
-        else
-        {
-            document.getElementById('outputSpecial').innerHTML = "";
-        }
+    }
+    if (tracker == testPassword.length)
+    {
+          document.getElementById('outputSpecial').innerHTML = "Password must include at least one special character (!,@,#, ect.).";
+          validationCheck = false;
+    }
+    else
+    {
+          document.getElementById('outputSpecial').innerHTML = "";
     }
 
     let search = testPassword.search(" ");
     if(search != -1){
         document.getElementById('outputSpace').innerHTML = "Password must not contain spaces.";
+        validationCheck = false;
     }
     else
     {
@@ -51,11 +61,13 @@ document.getElementById('click').onclick = function()
      if (upperPass == false)
      {
           document.getElementById('outputUpper').innerHTML = "Password must include at least one uppercase letter.";
+          validationCheck = false;
      }
      else
      {
           document.getElementById('outputUpper').innerHTML = "";
      }
+
 
      // -------------------- 5. Lower Case Letter --------------------
      let lowerPass = false;
@@ -72,11 +84,13 @@ document.getElementById('click').onclick = function()
      if (lowerPass == false)
      {
           document.getElementById('outputLower').innerHTML = "Password must include at least one lowercase letter.";
+          validationCheck = false;
      }
      else
      {
           document.getElementById('outputLower').innerHTML = "";
      }
+
 
      // -------------------- 6. Digit --------------------
      let numPass = 0;
@@ -92,11 +106,22 @@ document.getElementById('click').onclick = function()
      if (numPass === 0)
      {
           document.getElementById('outputDigit').innerHTML = "Password must include at least one numeric digit.";
+          validationCheck = false;
      }
      else
      {
           document.getElementById('outputDigit').innerHTML = "";
      }
 
+
+     // -------------------- Validation Confirmation --------------------
+     if(validationCheck === true)
+     {
+          document.getElementById('isValid').innerHTML = "Password is valid!";
+     }
+     else
+     {
+          document.getElementById('isValid').innerHTML = "Password is invalid:"
+     }
 
 }
